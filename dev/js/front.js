0b7d6b4f797cbd53f460ab4f60c4ae67ede3d303
Ruslan <cekip@mail.ru>
2021-04-14 16:41:35 +0600

    Complete: first pages block

--- dev/js/front.js
+++ dev/js/front.js
@@ -13,8 +13,38 @@ class Front extends _front{
       .on(_,'showPop')
       .on(_,'addSpace')
       .on(_,'closePop')
+      .on(_,'addFile')
+      .on(_,'removeFile')
       .add(_.componentName,'showForm',_.showCardFileForm.bind(_));
 
+  }
+  el(tag,params){
+    const _ = this;
+    if(params == undefined) params = {};
+    if (!tag) return null;
+    let
+        childes =  params['childes'] ?  params['childes']: null;
+    delete params['childes'];
+    let temp = document.createElement(tag);
+    if (tag == 'temp'){
+      temp = document.createDocumentFragment();
+    }
+    if(params){
+      for(let key in params){
+        if(key === 'text') {
+          if( (tag === 'INPUT') || (tag === 'TEXTAREA') ) temp.value = params[key];
+          else temp.textContent = params[key];
+        } else if(key === 'prop')  _[params[key]] = temp;
+        else if(key === 'html') temp.innerHTML = params[key];
+        else temp.setAttribute(`${key}`,`${params[key]}`);
+      }
+    }
+    if(  (childes instanceof  Array) && (childes.length) ) {
+      childes.forEach(function (el) {
+        temp.append(el);
+      });
+    }
+    return temp;
   }
   createOrderSuccess(orderData){}
   createOrderFail(orderData){}
@@ -60,7 +90,7 @@ class Front extends _front{
     if (!another){
       if (btn.classList.contains('select-option')){
         btn.parentElement.previousElementSibling.classList.add('choosen');
-      } else btn.parentElement.previousElementSibling.classList.remove('choosen');
+      } else btn.parentElement.classList.remove('choosen');
       if (select.firstElementChild.tagName === 'INPUT') select.firstElementChild.value = value;
       head.firstElementChild.textContent = value;
     }
@@ -95,5 +125,50 @@ class Front extends _front{
     }
   }
 
+  addFile(changeAction){
+    const _ = this;
+    let
+      input = changeAction.item;
+    _.changeFileInput(input);
+    let newAddButton = _.createFileAdded();
+    input.closest('.add-file-row').prepend(newAddButton);
+  }
+  changeFileInput(input){
+    const _ = this;
+    let
+      nextElem = input.nextElementSibling,
+      img = nextElem.firstElementChild,
+      cont = input.parentElement,
+      value = input.value;
+
+    value = value.split('\\');
+    value = value[value.length - 1];
+    let lastPosition = value.lastIndexOf('.');
+    let title = value.substring(0,lastPosition);
+    let type = value.substring(lastPosition + 1);
+
+    cont.className = 'add-file-added';
+    cont.setAttribute('data-click-action','Front:removeFile');
+    input.type = 'hidden';
+    img.src = `img/${type}.svg`;
+
+    let nameElem = _.el('SPAN',{text:title})
+    nextElem.append(nameElem)
+  }
+  createFileAdded(){
+    const _ = this;
+    let tpl = _.el('DIV',{
+      'class':'add-file','childes':[
+        _.el('INPUT',{'type':'file','data-change-action':'Front:addFile'}),
+        _.el('DIV',{'childes':[
+          _.el('IMG',{'src':'img/big-plus.svg'})
+        ]})
+      ]
+    });
+    return tpl;
+  }
+  removeFile(clickData){
+    clickData.item.remove();
+  }
 }
 new Front();
